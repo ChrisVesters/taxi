2ac47b95824941cf67e42cbc7afe49e5a9abccfd
Chris Vesters <vesters.chris@outlook.com>
2024-07-27 06:43:11 +0200

    Setup dispatcher service

---
+++ taxi-dispatcher/src/main/java/com/github/cvesters/taxi/dispatcher/TaxiDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.github.cvesters.taxi.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiDispatcherApplication.class, args);
+	}
+
+}
