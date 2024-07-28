3a186f0b3e434a62a76b06649690a82a13cfb10f
Chris Vesters <vesters.chris@outlook.com>
2024-07-28 09:55:26 +0200

    Extract location for re-usability.

--- taxi-dispatcher/src/test/java/com/github/cvesters/taxi/dispatcher/location/bdo/LocationTest.java
+++ taxi-dispatcher/src/test/java/com/github/cvesters/taxi/dispatcher/location/bdo/LocationTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.github.cvesters.taxi.dispatcher.location.bdo.Location;
+
 class LocationTest {
 
 	@Test
