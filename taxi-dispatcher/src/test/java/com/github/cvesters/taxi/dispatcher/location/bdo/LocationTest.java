e7cb6f344e969ce98debfa298f98119c0ab708e4
Chris Vesters <vesters.chris@outlook.com>
2024-07-29 15:09:00 +0200

    add translations

--- taxi-dispatcher/src/test/java/com/github/cvesters/taxi/dispatcher/location/bdo/LocationTest.java
+++ taxi-dispatcher/src/test/java/com/github/cvesters/taxi/dispatcher/location/bdo/LocationTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import com.github.cvesters.taxi.dispatcher.location.bdo.Location;
-
 class LocationTest {
 
 	@Test
